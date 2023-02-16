b0f2ca92c63b5bce4461b2c645494de0e56254d1
kim-thuita <kevinkoech265@gmail.com>
2023-02-16 15:57:09 -0800

    print-f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - function of the program
+*
+*Return: 0(success)
+*/
+
+int main(void)
+{
+	printf("/"with proper grammer, but the outcome is a piece of art, ");
+	return 0;
+}
+
