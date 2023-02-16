951f6836a6669e4fc153dc4b6f93dd4696553e71
Kevin Koech <105633302+kim-thuita@users.noreply.github.com>
2023-02-17 02:59:46 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - entry point
- * *  Return : (0) shows success
- */
+* main - main function
+*
+* Return: 0 (success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
