988408e1e7b020844c8d676f3cf7906eadb5e92d
Kevin Koech <105633302+kim-thuita@users.noreply.github.com>
2023-02-18 02:39:43 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
-/*if we may include other headers */
-
 /**
-*main -Entry point
-*
-*/
-int main (void)
-{
-	char c;
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 
-	for (c = 'a'; c <= 'z'; c++)
+int main(void)
+
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+
 	{
-		if(c != 'q' && c != 'e')
-		{
-			putchar(c);
-		}
-	putchar('\n');
+		if (n == 'e' || n == 'q')
+
+			continue;
+
+		putchar(n);
 	}
+	putchar('\n');
+
 	return (0);
 }
+
