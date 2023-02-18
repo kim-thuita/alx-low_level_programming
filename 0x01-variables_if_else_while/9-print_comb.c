1245fde9c06d1e136b744c13bb9376d0b7578d16
kim-thuita <kevinkoech265@gmail.com>
2023-02-18 00:09:12 -0800

    print

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main- Entry Point
+*
+*Return:0 (success)
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar('0' + a);
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
