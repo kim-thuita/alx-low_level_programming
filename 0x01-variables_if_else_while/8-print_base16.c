0de4cd2c6c970bdd4ec7fae15d2bd0966d3c102d
kim-thuita <kevinkoech265@gmail.com>
2023-02-17 20:20:31 -0800

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*main- Entry point
+*
+*Return:0 (success)
+*/
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar('0' + a);
+	}
+	for (b = 'a'; b <= 'f'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
+
