33455251f06c81148df171a47685a0f4f80ff906
Kevin Koech <105633302+kim-thuita@users.noreply.github.com>
2023-02-21 23:44:31 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,27 @@
 #include "main.h"
+
 /**
- * _abs - check the code.
- * @r: an input integer.
- * Return: absolute value.
+ * _abs - Entry Point
+ *
+ * Description: Return the absolute value of an interger
+ *
+ * @n: integer value to check and return its sign
+ *
+ * Return: 1, 0, or -1
  */
-int _abs(int r)
+
+int _abs(int n)
 {
-	if (r >= 0)
-		return (r);
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
 	else
-		return (r * -1);
+	{
+		return (-1 * n);
+	}
 }
