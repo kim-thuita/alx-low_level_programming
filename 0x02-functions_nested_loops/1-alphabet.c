0a87bb1adba94c179f83970af94523e27ab76b17
kim-thuita <kevinkoech265@gmail.com>
2023-02-21 06:33:35 -0800

    insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/**
+*print_alphabet - alphabet should be printed
+*
+*Return: 0 (success)
+*/
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+
+}
