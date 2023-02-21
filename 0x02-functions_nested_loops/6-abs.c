845c5cc2d4b1d4f5f8b5b14d172ef59fd13512cf
kim-thuita <kevinkoech265@gmail.com>
2023-02-21 12:19:21 -0800

    liverpool

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+ #include "main.h"
+
+/**
+ *_abs - Serves as the point
+ *
+ *Description: Return the absolute value of an integer
+ *
+ *@a: integer value to check and return its sign
+ *Return: 1, 0, or -1
+ */
+
+int _abs(int a)
+{
+	if (a > 0)
+	{
+		return (n);
+	}
+	else if (a == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		(-1 * n);
+	}
+
+}
