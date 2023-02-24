dec9112d4ecc1fbaf80a9ce9c2ac371b1e8e7056
kim-thuita <kevinkoech265@gmail.com>
2023-02-24 11:59:16 -0800

    Print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_line - prints the straight line
+ *
+ *@n: times straight line is printed
+ *
+ *Return: no return
+ */
+void print_line(int n)
+{
+	int  i;
+
+	if (n > 0)
+
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
