c56e7f1277f39ac3b4eb3b9e864bc5969e3392c5
kim-thuita <kevinkoech265@gmail.com>
2023-02-24 17:51:51 -0800

    prime_numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0 is success
+ */
+int main(void)
+{
+	long int a, b;
+
+	a = 612852475143;
+
+	for (b = 2; b <= a; b++)
+	{
+		if (a % b == 0)
+		{
+			a /= b;
+			b--;
+		}
+	}
+	printf("%d\n", b);
+	return (0);
+}
