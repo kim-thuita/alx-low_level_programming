c4bac9a70a75c2fd4024391597784ec786f5447e
kim-thuita <kevinkoech265@gmail.com>
2023-02-27 13:18:29 -0800

    Mispelled name

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ *print_rev - reversed string
+ *
+ *@s:string - char to printed
+ *
+ *Return: (0)
+ */
+void print_rev(char *s)
+{
+	char str = *s;
+
+	_putchar(strrev(str);
+
+	_putchar('\n');
+}
