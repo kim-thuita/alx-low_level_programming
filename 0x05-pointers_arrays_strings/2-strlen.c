92d684a110958c3f8160ad0c4fee740ca2ad6edc
kim-thuita <kevinkoech265@gmail.com>
2023-02-27 11:49:40 -0800

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strlen- get back length of a string
+ *
+ *@s:pointer deference
+ *Return: 0(success)
+ */
+
+int _strlen(char *s)
+{
+	_putchar(strlen(s));
+}
+
