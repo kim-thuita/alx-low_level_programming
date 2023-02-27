28db73be2fe10948cf3c74ce4ed794a3b69c8c0c
kim-thuita <kevinkoech265@gmail.com>
2023-02-27 10:43:23 -0800

    Reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - updates the value to the pointer
+ *
+ *@n: pointer variable.
+ *Description- Updating a pointer to display a new number
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# Working on pointers arrays and Strings
+1.Updating value using a pointer that has been declared
