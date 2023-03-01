2ca019a904938d6c89e7cc1d618f936cf65790b2
Kevin Koech <105633302+kim-thuita@users.noreply.github.com>
2023-03-02 01:00:35 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,33 @@
 #include "main.h"
+/**
+ * leet - Entry Point
+ *
+ * Description: Cypher Code :)
+ * @str: string to change encode
+ *
+ * Return: pointer to string
+ */
+
+char *leet(char *str)
+{
+	char *ptr = str;
+	int i;
+	char sub[] = {'A', 'E', 'O', 'T', 'L'};
+	char code[] = {'4', '3', '0', '7', '1'};
+
+	while (*str != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			printf("%c, %c\n", *str, *str + 32);
+			if ((*str == sub[i]) || ((*str - 32) == sub[i]))
+			{
+				*str = code[i];
+				break;
+			}
+		}
+		str++;
+	}
+
+	return (ptr);
+}
