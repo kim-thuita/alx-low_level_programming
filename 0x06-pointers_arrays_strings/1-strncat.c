119d5e2ce169de488a3c235ba6d33acd74e3c3c0
kim-thuita <kevinkoech265@gmail.com>
2023-03-01 11:18:47 -0800

    using n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strncat - concatenate two strings but add inputted numbers of bytes
+ *@dest: string to be appended upon
+ *@src: string to be completed at the end of dest
+ *@n:integer parameter to compare index to
+ *Return: return new concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int index = 0;
+
+	int dest_len = 0;
+
+	while (dest[index++])
+
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
