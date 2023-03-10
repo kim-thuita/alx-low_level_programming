4100b6740675e07f90165d3bc5769aec052833e4
kim-thuita <kevinkoech265@gmail.com>
2023-03-10 09:17:35 -0800

    DONE

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - print the name of the program
+ *@argc: number of commands
+ *@argv: array of commands
+ *Return: Always 0.
+ */
+
+int main(int argc, char ***argv)
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	exit(EXIT_SUCCESS);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+#ARGVS AND ARGC
