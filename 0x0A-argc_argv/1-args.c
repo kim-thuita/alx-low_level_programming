e38ee07170d9735e64bc2e79b687962683007bc0
kim-thuita <kevinkoech265@gmail.com>
2023-03-10 09:54:35 -0800

    fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,19 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
+ *main-prints number of args
+ *@argc: arguement count
+ *@argv: arguement vector
  *prints number of arguements passed to it
  */
 
 int main(int argc, char *argv[])
 {
-	int i = 0;
+	(void) argv;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%d\n", argv[i]);
-	}
+	printf("%d\n", argc - 1);
+
+	return(0);
 }
 
