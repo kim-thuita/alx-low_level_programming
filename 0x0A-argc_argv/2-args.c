861346ceac60b2beac31ee8a924561098b507242
kim-thuita <kevinkoech265@gmail.com>
2023-03-10 10:31:59 -0800

    arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - prints all arguements it receives
+ *@argc: arguemented counter
+ *@argv: list of what is pinted
+ *Return: 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n",argv[i]);
+	}
+
+	return (0);
+}
